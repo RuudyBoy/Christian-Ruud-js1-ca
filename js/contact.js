46d548703215299f05676c60148ce84efc2b6a73
RuudyBoy <73653923+RuudyBoy@users.noreply.github.com>
2021-04-28 16:18:48 +0200

    hei

--- js/contact.js
+++ js/contact.js
@@ -13,4 +13,60 @@ const address = document.querySelector("#address");
 const addressError = document.querySelector("#addressError");
 
 
-function forum {}
+
+
+form.onsubmit = function (event) {
+    event.preventDefault();
+
+
+    if (checkLength(fullName.value, 0)) {
+        fullNameError.style.display = "none";
+        
+    } else {
+        fullNameError.style.display ="block";
+      
+    }
+
+    if (checkLength(subject.value, 10)) {
+        subjectError.style.display = "none";
+       
+
+    } else {
+        subjectError.style.display ="block";
+        
+        
+    }
+
+    if (emailForm(email.value)) {
+        emailError.style.display = "none"
+       
+
+    } else {
+        emailError.style.display = "block";
+       
+    }
+
+    if (checkLength(address.value, 25)) {
+        addressError.style.display = "none"
+      
+
+    } else {
+        addressError.style.display = "block"
+    }
+}
+
+function checkLength(value, len) {
+
+    if (value.trim().lenght > len)  {
+        return true;
+    } else {
+        return false;
+    }
+    
+}
+
+function emailForm(email) {
+    const regEx = /\S+@\S+\.\S+/;
+    const matches = regEx.test(email);
+    return matches;
+}
