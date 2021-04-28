7200edcdfd1df5321b9e8dbee8b4c4ffe094ecce
RuudyBoy <73653923+RuudyBoy@users.noreply.github.com>
2021-04-28 17:15:51 +0200

    yeah

--- js/details.js
+++ js/details.js
@@ -1,38 +1,32 @@
 
-
 const queryString = document.location.search;
-
-console.log(queryString);
 
 const params = new URLSearchParams(queryString);
 
 const id = params.get("id");
 
-console.log(id);
+
+const url = "https://www.easports.com/fifa/ultimate-team/api/fut/item/" + id;
+const corsUrl = "https://noroffcors.herokuapp.com/" + url;
 
 
 const detailsContainer = document.querySelector(".details-container");
-
 const idContainer = document.querySelector(".id");
 idContainer.innerHTML = id;
 
-const url = "https://free-nba.p.rapidapi.com/players?page=0&per_page=25" + id;
 
-const apiKey =  {"headers": {
-    "x-rapidapi-key": "623b2d79a6msh38ce6bc44c7bb1ap1aaf80jsn6fca4a0052bc",
-    "x-rapidapi-host": "free-nba.p.rapidapi.com"
-}};
+detailsContainer.innerHTML = "";
 
-async function gameStats () {
+
+async function fetchBook () {
     try {
 
-        const response = await fetch(url, apiKey);
+        const response = await fetch(corsUrl);
         const details = await response.json();
 
         console.log(details);
 
         createHTML(details);
-
 
     }
     
@@ -42,13 +36,17 @@ async function gameStats () {
  }
 }
 
-gameStats ();
-
-function createHTML(details){
+fetchBook ();
 
 
+function createHTML (details) {
 
+    document.title = `${details.type}`;
+
+    detailsContainer.innerHTML = `<div class="details_container"> ${details.type} ${details.count} ${details.totalResults}  </div>` ;
+    
 }
+
 
 
 
--- js/index.js
+++ js/index.js
@@ -1,40 +1,39 @@
 
-const url = "https://free-nba.p.rapidapi.com/players?page=0&per_page=25";
-
-const apiKey =  {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "623b2d79a6msh38ce6bc44c7bb1ap1aaf80jsn6fca4a0052bc",
-		"x-rapidapi-host": "free-nba.p.rapidapi.com"
-	}};
-
 const container = document.querySelector(".container");
+const url = "https://www.easports.com/fifa/ultimate-team/api/fut/item";
+const corsUrl = "https://noroffcors.herokuapp.com/" + url;
 
-async function apiCall () {
+ container.innerHTML = "";
+
+  async function apiCall () {
 
     try {
+    
+    const result = await fetch(corsUrl)
+  
+    const data = await result.json();
 
-        const response = await fetch(url, apiKey);
-        const basket = await response.json();
+    const hoy = data.items;
 
-        const results = basket.data;
-        console.log(basket);
+    console.log(hoy);
 
-        
-        container.innerHTML = "";
+    
+    for (let i = 0; i < hoy.length; i++) {
 
-        for (let i = 0; i < results.length; i++) {
-           
-            container.innerHTML += `<a href="details.html?id=${results[i].id}" class="card"> <div class="container"> basket league:${results[i].team.full_name} <div> </a> `;
-        }
- }      catch (error) {
-     console.log("error");
-     container.innerHTML = "An error has occured :("
+      if (i === 15) {
+        break
+      }
+
+
+    container.innerHTML += `<a class="card" href="details.html?id=${i}">${i} </a> `;
  }
 
+  } catch (error) {
+    
+    console.log("error");
 
+    }
 
-        
 }
 
 apiCall();
