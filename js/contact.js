bd4486b0beb35479db72870d6d51e9fe67a6eae9
RuudyBoy <73653923+RuudyBoy@users.noreply.github.com>
2021-04-28 12:34:32 +0200

    j

---
+++ js/contact.js
@@ -0,0 +1,16 @@
+const form = document.querySelector("form");
+
+const fullName = document.querySelector("#fullName");
+const fullNameError = document.querySelector("#fullNameError");
+
+const subject = document.querySelector("#subject");
+const subjectError = document.querySelector("#subjectError");
+
+const email = document.querySelector("#email");
+const emailError = document.querySelector("#emailError");
+
+const address = document.querySelector("#address");
+const addressError = document.querySelector("#addressError");
+
+
+function forum {}
